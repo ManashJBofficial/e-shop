0dca3e590df61e4c167b4d7be5484b32a230aa7d
ManashJBofficial <mjbaruah10@gmail.com>
2021-07-25 23:02:30 +0530

    added reset to user profiles data

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,7 +1,7 @@
 import  { ORDER_CREATE_REQUEST,ORDER_CREATE_SUCCESS,ORDER_CREATE_FAIL,
             ORDER_DETAILS_REQUEST,ORDER_DETAILS_SUCCESS,ORDER_DETAILS_FAIL,
             ORDER_PAY_REQUEST,ORDER_PAY_SUCCESS,ORDER_PAY_FAIL,ORDER_PAY_RESET,
-            ORDER_SHOW_MY_LIST_REQUEST,ORDER_SHOW_MY_LIST_SUCCESS,ORDER_SHOW_MY_LIST_FAIL
+            ORDER_SHOW_MY_LIST_REQUEST,ORDER_SHOW_MY_LIST_SUCCESS,ORDER_SHOW_MY_LIST_FAIL,ORDER_SHOW_MY_LIST_RESET
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -62,7 +62,9 @@ export const orderShowMyListReducer = (state = { orders:[]},action) => {
       case ORDER_SHOW_MY_LIST_SUCCESS:
           return { loading:false, orders: action.payload }    
       case ORDER_SHOW_MY_LIST_FAIL:
-          return { loading:false, error:action.payload }    
+          return { loading:false, error:action.payload }  
+      case ORDER_SHOW_MY_LIST_RESET:
+          return { orders:[] }  
       default:
           return state
   }
