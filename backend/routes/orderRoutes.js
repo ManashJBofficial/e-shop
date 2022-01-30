ea1579c226adb44cc1b5807a7c8cb0b08d173506
ManashJBofficial <mjbaruah10@gmail.com>
2022-01-30 13:56:41 +0530

    admin get orderlist api endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 const router = express.Router()
-import {addOrderItems,getMyOrders,getOrderById, updateOrderToPaid } from '../controllers/orderController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import {addOrderItems,getMyOrders,getOrderById, updateOrderToPaid,getOrders, } from '../controllers/orderController.js'
+import { protect,checkAdmin } from '../middleware/authMiddleware.js'
 
-router.route('/').post(protect, addOrderItems)
+router.route('/').post(protect, addOrderItems).get(protect, checkAdmin, getOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
