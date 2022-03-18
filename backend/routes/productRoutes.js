b34466da5b8e1d12da77bfa4d964bdded577658d
Manash Jyoti Baruah <mjbaruah10@gmail.com>
2022-03-18 11:51:03 +0530

    backend api for carousel products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,19 +1,28 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 import {
   getProducts,
   getProductById,
   deleteProduct,
   createProduct,
   updateProduct,
-  createProductReview
-} from '../controllers/productController.js'
-import { protect, checkAdmin } from '../middleware/authMiddleware.js'
+  createProductReview,
+  getTopProducts,
+} from "../controllers/productController.js";
+import { protect, checkAdmin } from "../middleware/authMiddleware.js";
 
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductById).delete(protect, checkAdmin, deleteProduct)
-router.route('/').get(getProducts).post(protect, checkAdmin, createProduct)
-router.route('/:id').get(getProductById).delete(protect, checkAdmin, deleteProduct).put(protect, checkAdmin, updateProduct)
-router.route('/:id/reviews').post(protect, createProductReview)
+router.route("/").get(getProducts);
+router.get("/top", getTopProducts);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, checkAdmin, deleteProduct);
+router.route("/").get(getProducts).post(protect, checkAdmin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, checkAdmin, deleteProduct)
+  .put(protect, checkAdmin, updateProduct);
 
-export default router
+export default router;
